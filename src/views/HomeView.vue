a1f264c2b97aa7b55b5e27656b9b6b9346377ac8
assassinhua <2943961353@qq.com>
2024-10-24 10:02:42 +0800

    7

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue'
+</script>
+
 
 <template>
-  <div class="home"></div>
+<div class="home">
+    <EventCard />
+  </div>
+
  </template>
 
 
